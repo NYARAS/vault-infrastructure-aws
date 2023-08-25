ad48f42de3aa304c033fc4fd9ca448238b0dc322
Calvine Otieno <35563516+NYARAS@users.noreply.github.com>
2023-08-25 18:27:11 +0200

    chore: configure database secret engine connection (#19)
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- terraform-vault-configuration/main.tf
+++ terraform-vault-configuration/main.tf
@@ -23,3 +23,86 @@ resource "vault_mount" "db" {
   type = "database"
   description = "Dynamic Secrets Engine for Test Postgresql DB."
 }
+
+
+// Configure database secret engine connection [PSQL]
+resource "vault_database_secret_backend_connection" "postgres" {
+  backend       = vault_mount.db.path
+  name          = "postgresql"
+  allowed_roles = ["service-write", "dev-read"]
+
+  postgresql {
+    connection_url = "postgresql://${var.DB_USER}:${var.DB_PASSWORD}@${var.DB_URL}/${var.DB}?sslmode=disable"
+    
+  }
+}
+
+resource "vault_database_secret_backend_role" "postgres_service_write" {
+  backend = vault_mount.db.path
+  name    = "service-write"
+  db_name = vault_database_secret_backend_connection.postgres.name
+
+  creation_statements = [
+    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}';",
+    "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\";",
+  ]
+
+
+  revocation_statements = [
+    "REVOKE ALL ON ALL TABLES IN SCHEMA public FROM \"{{name}}\";",
+    "DROP ROLE \"{{name}}\";",
+  ]
+
+  default_ttl = 864000 // 10 days
+  max_ttl     = 864000 // 10 days
+}
+
+resource "vault_database_secret_backend_role" "postgres_dev_read" {
+  backend = vault_mount.db.path
+  name    = "dev-read"
+  db_name = vault_database_secret_backend_connection.postgres.name
+
+  creation_statements = [
+    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}';",
+    "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";",
+  ]
+
+
+  revocation_statements = [
+    "REVOKE ALL ON ALL TABLES IN SCHEMA public FROM \"{{name}}\";",
+    "DROP ROLE \"{{name}}\";",
+  ]
+
+
+  default_ttl = 2592000 // 30 days
+  max_ttl     = 2592000 // 30 days
+}
+
+
+resource "vault_policy" "reader_policy" {
+  name = "reader"
+  policy = data.vault_policy_document.reader_policy.hcl
+}
+
+resource "vault_auth_backend" "kubernetes" {
+  type = "kubernetes"
+}
+
+resource "vault_kubernetes_auth_backend_role" "app" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "app"
+  bound_service_account_names      = ["app"]
+  bound_service_account_namespaces = ["app"]
+  token_ttl                        = 86400
+  token_policies                   = ["reader"]
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", "${data.terraform_remote_state.eks.outputs.cluster_name}"]
+    command     = "aws"
+  }
+}
