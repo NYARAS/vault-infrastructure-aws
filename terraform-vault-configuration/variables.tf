cc24903f4695a11c632aee1cb8afd610858443ea
Calvine Otieno <nyarangaotieno@gmail.com>
2023-08-25 19:20:47 +0300

    chore: enable PSQL database secret engine
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ terraform-vault-configuration/variables.tf
@@ -0,0 +1,19 @@
+variable "DB_USER" {
+  description = "postgresql username"
+  default = "postgres"
+}
+
+variable "DB_PASSWORD" {
+  description = "postgresql password"
+  default = "password"
+}
+
+variable "DB_URL" {
+  description = "postgresql URL"
+  default = "postgres.default:5432"
+}
+
+variable "DB" {
+  description = "postgresql URL"
+  default = "test"
+}
