a60cf2e8ee377007bb6204a40990d47f0c9dcd5c
Calvine Otieno <35563516+NYARAS@users.noreply.github.com>
2025-01-04 22:02:30 +0200

    chore: added kms config (#52)
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- vault.tf
+++ vault.tf
@@ -18,20 +18,22 @@ resource "helm_release" "csi" {
 
 resource "helm_release" "vault" {
   depends_on = [
-    helm_release.consul,
     helm_release.csi
     ]
   name      = "vault"
   repository = "https://helm.releases.hashicorp.com"
   chart      = "vault"
   namespace = kubernetes_namespace.vault.metadata.0.name
-  version   = "0.24.1"
+  version   = "0.29.0"
   values = [
     templatefile(
       "vault-helm/values.tmpl",
       {
       "replicas" = var.vault_node_count
       "vault_server_host"              = var.vault_server_host
+       kms_key_id              = aws_kms_key.vault.key_id
+       region = var.region
+       vault_iam_role_arn      = module.vault_service_account_role.iam_role_arn
       }
       )
   ]
@@ -45,5 +47,50 @@ data "kubernetes_service" "vault_svc" {
   metadata {
     namespace = "vault"
     name      = "vault-ui"
+  }
+}
+resource "aws_iam_policy" "vault_service_account_policy" {
+  name        = var.service_account_policy_name
+  path        = "/"
+  description = "Service account policy for Vault in EKS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ]
+        Effect   = "Allow"
+        Resource = aws_kms_key.vault.arn
+      }
+    ]
+  })
+
+  tags = {
+    Name = var.service_account_policy_name
+  }
+}
+
+# AWS IAM role used for the Vault service account
+module "vault_service_account_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = var.service_account_role_name
+  role_policy_arns = {
+    vault_service_account_policy = aws_iam_policy.vault_service_account_policy.arn
+  }
+
+  oidc_providers = {
+    default = {
+      provider_arn               = data.terraform_remote_state.eks.outputs.oidc_provider_arn
+      namespace_service_accounts = ["${var.namespace}:vault"]
+    }
+  }
+
+  tags = {
+    Name = var.service_account_role_name
   }
 }
