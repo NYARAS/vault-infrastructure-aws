f16fd686311b4cfe19d03761b9c0e3becd951bf5
Calvine Otieno <35563516+NYARAS@users.noreply.github.com>
2025-01-05 21:21:17 +0200

    cleanup: remove remote backend data config (#56)
    Signed-off-by: Calvine Otieno <calvineotieno010@gmail.com>
    Co-authored-by: Calvine Otieno <calvineotieno010@gmail.com>

--- vault-configuration/data.tf
+++ vault-configuration/data.tf
@@ -11,10 +11,5 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
 
   config = {
-    bucket               = "devops-demo.tfstate"
-    key                  = "argocdinfra.json"
-    region               = "eu-west-1"
-    workspace_key_prefix = "environment"
-    dynamodb_table       = "devops-demo.tfstate.lock"
   }
 }
